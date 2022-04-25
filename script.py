7b3c6f3f87d2915651b4281ef137e772a22d947a
João Gabriel Moreira Bossatto Daré <79703333+Joaogmbd@users.noreply.github.com>
2022-04-25 16:23:31 -0300

    Update script.py

--- script.py
+++ script.py
@@ -3,9 +3,9 @@ import sys
 
 def mover(o, d):
     move(o, d)
-    print('arquivo movido com sucesso.')
 
 try:
     mover(sys.argv[1], sys.argv[2])
+    print('O arquivo movido com sucesso.')
 except:
-    print('um dos parâmetros foi passado incorretamente.')
+    print('Um dos parâmetros foi passado incorretamente.')
